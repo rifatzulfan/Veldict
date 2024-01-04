ad94b6eed0135d3e4915c68a66abe3f9f424869d
rifat <rifatzulfanmanaf@gmail.com>
2024-01-04 14:29:35 +0700

    sampe login modal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+$(document).ready(function () {
+  $(".my-slider").each(function () {
+    $(this).slick({
+      slidesToScroll: 1,
+      arrows: true,
+      dots: false,
+      speed: 300,
+      infinite: false,
+      autoplay: false,
+      variableWidth: true,
+      responsive: [
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
+    });
+  });
+});
+
 $(".click").click(function () {
   var $heartIcon = $(this).find("span");
 
@@ -33,3 +61,26 @@ $(".click").click(function () {
     }, 1000);
   }
 });
+
+// Implementasi JavaScript untuk meng-handle accordion
+document.addEventListener("DOMContentLoaded", function () {
+  function setupPanel(panelNumber) {
+    var panelCheckbox = document.getElementById("panel-" + panelNumber);
+    var panelTrigger = document.getElementById(
+      "panel-" + panelNumber + "-trigger"
+    );
+
+    panelTrigger.addEventListener("click", function () {
+      panelCheckbox.checked = !panelCheckbox.checked;
+    });
+  }
+
+  // Setup each panel
+  setupPanel(1);
+  setupPanel(2);
+  setupPanel(3);
+});
+
+function toggleModal() {
+  document.getElementById("modal").classList.toggle("hidden");
+}
